1867913e19c65d22fb99e7aff130315fcfe50b71
Arpine Harutyunyan <54270986+arpih@users.noreply.github.com>
2021-02-24 10:38:39 +0400

    New translations lang.js (Spanish)

--- src/lang/es.js
+++ src/lang/es.js
@@ -1,1 +1,5 @@
-export default { startWelcome: 'crwdns1210:0crwdne1210:0' };
+export default {
+    startWelcome: 'crwdns1210:0crwdne1210:0',
+    loginSignOut: 'crwdns1212:0crwdne1212:0',
+    loginSignIn: 'crwdns1214:0crwdne1214:0'
+};
