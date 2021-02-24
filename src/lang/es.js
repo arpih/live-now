6f9e5f6645710a8a2b9e8b32c9294f4b18b014b8
Arpine Harutyunyan <arpinesasunih@gmail.com>
2021-02-24 22:32:01 +0400

    Update strings

---
+++ src/lang/es.js
@@ -0,0 +1,6 @@
+export default {
+  loginWelcome: 'crwdns1216:0crwdne1216:0',
+  loginSignIn: 'crwdns1218:0crwdne1218:0',
+  loginSignOut: 'crwdns1220:0crwdne1220:0',
+  loginMessage: 'crwdns1222:0crwdne1222:0',
+};
--- src/lang/hy.js
+++ src/lang/hy.js
@@ -1,6 +1,6 @@
 export default {
   loginWelcome: 'Ողջույն',
   loginSignIn: 'Մուտք գործել',
-  loginSignOut: '',
-  loginMessage: 'Would you like to share your moment?',
+  loginSignOut: 'Դուրս գալ',
+  loginMessage: 'Ցանկանու՞մ եք կիսվել ձեր պահը։',
 };
