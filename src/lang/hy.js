874f20cc096890feaec2aa674c589f2a0433877a
Arpine Harutyunyan <54270986+arpih@users.noreply.github.com>
2021-02-24 12:41:26 +0400

    New translations lang.js (Armenian)

--- src/lang/hy.js
+++ src/lang/hy.js
@@ -2,5 +2,5 @@ export default {
     loginWelcome: 'Բարի գալուստ',
     loginSignIn: 'Մուտք գործել',
     loginSignOut: 'Sign Out',
-    loginMessage: 'Would you like to share your moment?'
+    loginMessage: 'Կցանկանայի՞ք կիսվել ձեր պահով:'
 };
